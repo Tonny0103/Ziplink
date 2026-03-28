33550ff2d4ea55e31d1a4a9d12c966f822126a1e
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-03-28 18:58:58 -0400

    Initial commit

---
+++ src/test/java/com/company/ziplink/ZiplinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ziplink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiplinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
