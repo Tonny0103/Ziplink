7380a3bcf05de239246c19588b88058d9f9083cf
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-03-29 17:42:19 -0400

    feat(User): created user model

---
+++ src/main/java/com/company/ziplink/models/User.java
@@ -0,0 +1,34 @@
+package com.company.ziplink.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name", columnDefinition = "text")
+    private String name;
+
+    @Column(name = "email", columnDefinition = "text", unique = true)
+    private String email;
+
+    @Column(name = "password", columnDefinition = "text", unique = true)
+    private String password;
+
+    @Column(name = "created_at", columnDefinition = "timestamp", insertable = false, updatable = false)
+    private Date createdAt;
+}
