111ae0eb972a368ea06accadf7b71e06d363a349
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-08 16:10:33 -0400

    test(UserServiceTests): Green - Created basic logic to run the tests

---
+++ src/main/java/com/company/ziplink/services/UserService.java
@@ -0,0 +1,15 @@
+package com.company.ziplink.services;
+
+import com.company.ziplink.models.User;
+import com.company.ziplink.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User createUser(User user) { return userRepository.save(user); }
+}
