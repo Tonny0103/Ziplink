82cca56413c9090907a63c02229fb6d7a1644d78
Anthonny Gabriell Marins Alves <anthonny14gabriell@gmail.com>
2026-04-08 15:59:01 -0400

    test(UserServiceTests): Red - Creating test to assert that new user must have unique email

--- src/test/java/com/company/ziplink/services/UserServiceTests.java
+++ src/test/java/com/company/ziplink/services/UserServiceTests.java
@@ -1,4 +1,48 @@
 package com.company.ziplink.services;
 
+import com.company.ziplink.models.User;
+import com.company.ziplink.repositories.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Date;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTests {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    void mustCreateUserWithUniqueEmail() {
+        when(userRepository.existsUserByEmail(fakeUser().getEmail())).thenReturn(false);
+        when(userRepository.save(fakeUser())).thenReturn(fakeUser());
+
+        User result = userService.createUser(fakeUser());
+
+        assertThat(result.getName()).isEqualTo("John Doe");
+        assertThat(result.getEmail()).isEqualTo("johndoe67@gmail.com");
+        verify(userRepository).save(any(User.class));
+    }
+
+    private User fakeUser() {
+        User user = new User();
+        user.setId(UUID.randomUUID());
+        user.setName("John Doe");
+        user.setEmail("johndoe67@gmail.com");
+        user.setPassword("test123");
+        user.setCreatedAt(new Date());
+        return user;
+    }
 }
